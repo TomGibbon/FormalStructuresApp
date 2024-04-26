21764c99e36d303c2ba1f088590fd3d218d3e292
TomGibbon <tomgibbon222@gmail.com>
2024-04-26 17:24:14 +0100

    changed test command to work with mac intel

--- testcpp.sh
+++ testcpp.sh
@@ -1,1 +1,5 @@
-(cd ios && g++ -std=c++11 -stdlib=libc++ -I/opt/homebrew/Cellar/opencv/4.9.0_3/include/opencv4 -L/opt/homebrew/Cellar/opencv/4.9.0_3/lib -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_videoio -lopencv_features2d -lopencv_dnn -lopencv_ml -I/opt/homebrew/Cellar/openssl@3/3.2.1/include -L/opt/homebrew/Cellar/openssl@3/3.2.1/lib -lssl -lcrypto -I/opt/homebrew/Cellar/tesseract/5.3.4/include -L/opt/homebrew/Cellar/tesseract/5.3.4/lib -ltesseract -I/opt/homebrew/Cellar/leptonica/1.84.1/include -L/opt/homebrew/Cellar/leptonica/1.84.1/lib -lleptonica mainCode.cpp tester.cpp -o tester && ./tester)
+# Apple silicon machines
+# (cd ios && g++ -std=c++11 -stdlib=libc++ -I/opt/homebrew/Cellar/opencv/4.9.0_3/include/opencv4 -L/opt/homebrew/Cellar/opencv/4.9.0_3/lib -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_videoio -lopencv_features2d -lopencv_dnn -lopencv_ml mainCode.cpp tester.cpp -o tester && ./tester)
+
+# Mac Intel machines
+(cd ios && g++ -std=c++11 -stdlib=libc++ -I/usr/local/Cellar/opencv/4.9.0_7/include/opencv4/ -L/usr/local/Cellar/opencv/4.9.0_7/lib -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_videoio -lopencv_features2d -lopencv_dnn -lopencv_ml mainCode.cpp tester.cpp -o tester && ./tester)
